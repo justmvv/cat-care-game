ff1a2a3128d3e343be8eabb892abfb01cab09943
Vasilii Morozov <just.mvv@gmail.com>
2026-07-18 22:23:17 +0300

    Fixgameplay 2

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"83675ed27633283e7fc296c8bca22e841224c0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "845357847" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3152658377" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
